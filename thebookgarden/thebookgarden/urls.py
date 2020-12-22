153bfa229cd897b19b9e0827acc06dbe63558174
Arsalan Jamali <jamaliarsalan@yahoo.com>
2020-12-23 00:43:17 +0500

    Admin Site was Set

--- thebookgarden/thebookgarden/urls.py
+++ thebookgarden/thebookgarden/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include
 from .views import HomePage,ContactUsView,AboutUsView,CartDisplayView
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,5 +32,9 @@ urlpatterns = [
 ]
 
 urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "The Book Garden"
+admin.site.site_title = "TBG Admin Portal"
+admin.site.index_title = "Welcome to The Book Garden Admin Portal"
 
 
